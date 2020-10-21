7f743294d52b8b9e2415f30f0f746cefa45bc33c
Awmfara <69292567+Awmfara@users.noreply.github.com>
2020-10-22 01:31:45 +1100

    Updated Sewer Scene

---
+++ Assets/Scripts/Sewer/CatCatcher.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spawner;
+
+public class CatCatcher : MonoBehaviour
+{
+    private SpriteRenderer catSR;
+
+    public GameObject player;
+
+    [SerializeField, Tooltip("1=Right,-1=Left")]
+    private int dir = 1;
+    [SerializeField]
+    private float moveSpeed = 100f;
+
+    public float distance = 5;
+    public float minDistance = 1;
+
+
+    private void Start()
+    {
+        catSR = gameObject.GetComponent<SpriteRenderer>();
+   
+        dir = 1;
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        
+}
+    // Update is called once per frame
+    void Update()
+
+    {
+        SpawnDirection();
+
+        transform.position = new Vector2(transform.position.x + Time.deltaTime * dir * moveSpeed, transform.position.y);
+    }
+
+    private void SpawnDirection()
+    {
+        
+
+        if (transform.position.y > 44)
+        {
+            if (transform.position.x < 0)
+            {
+                dir = 1;
+                catSR.flipX = false;
+            }
+           if(transform.position.x>0)
+            {
+                dir = -1;
+                catSR.flipX = true;
+            }
+
+        }
+    }
+}
--- Assets/Scripts/Sewer/TransportEdge.cs
+++ Assets/Scripts/Sewer/TransportEdge.cs
@@ -1,23 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
 using UnityEngine;
 
-
-public class TransportEdge : MonoBehaviour
+namespace Spawner
 {
-    [SerializeField]
-    private GameObject[] spawnPoints;
-    int spawn=0;
-    private void Start()
+    public class TransportEdge : MonoBehaviour
     {
-      spawn = Random.Range(0, spawnPoints.Length);
-    }
+        [SerializeField, Tooltip("Assign to Spawn Points")]
+        private GameObject[] spawnPoints = null;
+        [SerializeField]
+        private GameObject[] bounds = null;
+        [SerializeField]
+        private float distanceL = 0;
+        [SerializeField]
+        private float distanceR = 0;
+        [SerializeField]
+        private float setDistance = 0;
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
 
-            transform.position = spawnPoints[spawn].transform.position;
-            spawn = Random.Range(0, spawnPoints.Length);
- 
+        [SerializeField, Tooltip("Which SpawnPoint Object will Spawn to")]
+        public int spawnIndex = 0;
+        private void Start()
+        {
+            spawnIndex = Random.Range(0, spawnPoints.Length);
+            distanceL= Vector2.Distance(transform.position, bounds[0].transform.position);
+            distanceR = Vector2.Distance(transform.position, bounds[1].transform.position);
+        }
+        private void Update()
+        {
+            distanceL = Vector2.Distance(transform.position, bounds[0].transform.position);
+            distanceR = Vector2.Distance(transform.position, bounds[1].transform.position);
+            if (distanceL<=setDistance||distanceR<=setDistance)
+            {
+                transform.position = spawnPoints[spawnIndex].transform.position;
+                spawnIndex = Random.Range(0, spawnPoints.Length);
+            }
+        }
+
+        /// <summary>
+        /// When Object Collides with Collider object spawns to random spawn point
+        /// </summary>
+        /// <param name="collision"></param>
+        //private void OnTriggerEnter2D(Collider2D boundC)
+        //{
+        //    if (boundC.CompareTag("Bound"))
+        //    {
+        //        transform.position = spawnPoints[spawnIndex].transform.position;
+        //        spawnIndex = Random.Range(0, spawnPoints.Length); 
+        //    }
+            
+
+        //}
     }
 }
