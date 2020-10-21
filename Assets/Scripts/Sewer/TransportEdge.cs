20ca6bf33ed6ebf8ec031f7e788a7d8f96780e08
Awmfara <69292567+Awmfara@users.noreply.github.com>
2020-10-21 13:36:41 +1100

    SewerSceneTest

---
+++ Assets/Scripts/Sewer/TransportEdge.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TransportEdge : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] spawnPoints;
+    int spawn=0;
+    private void Start()
+    {
+      spawn = Random.Range(0, spawnPoints.Length);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+            transform.position = spawnPoints[spawn].transform.position;
+            spawn = Random.Range(0, spawnPoints.Length);
+ 
+    }
+}
