6484964f65825d0393f1042695d22fbd0b8371bb
Awmfara <69292567+Awmfara@users.noreply.github.com>
2020-10-22 21:58:19 +1100

    Enemy
    Added change direction when stuck

--- Assets/Scripts/Sewer/CatCatcher.cs
+++ Assets/Scripts/Sewer/CatCatcher.cs
@@ -14,6 +14,13 @@ public class CatCatcher : MonoBehaviour
     [SerializeField]
     private float moveSpeed = 100f;
 
+    public float catPoint1;
+    public float catPoint2;
+    public float catPointCountdown1 = 4;
+    public float catPointCountdown2 = 2;
+    public float a = 1;
+    public float test;
+
     public float distance = 5;
     public float minDistance = 1;
 
@@ -21,23 +28,58 @@ public class CatCatcher : MonoBehaviour
     private void Start()
     {
         catSR = gameObject.GetComponent<SpriteRenderer>();
-   
+
         dir = 1;
         distance = Vector2.Distance(transform.position, player.transform.position);
-        
-}
+
+    }
     // Update is called once per frame
     void Update()
 
     {
         SpawnDirection();
+        TurnArouncCatcher();
 
         transform.position = new Vector2(transform.position.x + Time.deltaTime * dir * moveSpeed, transform.position.y);
     }
 
+    private void TurnArouncCatcher()
+    {
+        catPointCountdown1 -= Time.deltaTime;
+        catPointCountdown2 -= Time.deltaTime;
+
+        if (catPointCountdown1 <= 0)
+        {
+            catPointCountdown1 = 3;
+            catPoint1 = transform.position.x;
+
+        }
+        if (catPointCountdown2 <= 0)
+        {
+            catPointCountdown2 = 4;
+            catPoint2 = transform.position.x;
+
+        }
+        test = catPoint2 - catPoint1;
+        if (test <= 10 && test >= -10)
+        {
+            catPoint2 = 1000;
+            if (dir == 1)
+            {
+                dir = -1;
+                catSR.flipX = true;
+            }
+            else if (dir == -1)
+            {
+                dir = 1;
+                catSR.flipX = false;
+            }
+        }
+    }
+
     private void SpawnDirection()
     {
-        
+
 
         if (transform.position.y > 44)
         {
@@ -46,7 +88,7 @@ public class CatCatcher : MonoBehaviour
                 dir = 1;
                 catSR.flipX = false;
             }
-           if(transform.position.x>0)
+            if (transform.position.x > 0)
             {
                 dir = -1;
                 catSR.flipX = true;
