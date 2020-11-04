e1fd951f5c1e1825214e30e0b9cbb7efc486b4d3
mdk-1 <mdkin2006@gmail.com>
2020-11-04 22:16:31 +1100

    Finalizing level 3
    added catcatcher script
    added catcatcher sprite
    finalized level 3

---
+++ Assets/Scripts/LevelThreeScripts/CatCatcherManagerLevelThree.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatCatcherManagerLevelThree : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject bloodEffect;
+    private float aiSpeed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+     this.transform.position = Vector2.MoveTowards(this.transform.position, new Vector2(player.transform.position.x, transform.position.y), aiSpeed * Time.deltaTime);
+
+
+    }
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Instantiate(bloodEffect, player.transform.position, Quaternion.identity);
+            GameManager.instance.health = -1;
+        }
+    }
+
+}
