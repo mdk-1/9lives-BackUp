749727dd32736956c346790956222e38bb80b377
Awmfara <69292567+Awmfara@users.noreply.github.com>
2020-11-06 13:20:23 +1100

    Upadted Music

---
+++ Assets/Scripts/IntroSceneScripts/IntroSceneAudio.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroSceneAudio : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioSource typewriter;
+    [SerializeField]
+    private AudioSource meow;
+    public bool typingOn;
+    public bool meowOn;
+
+
+    private void Awake()
+    {
+        typingOn = false;
+
+    }
+    private void Update()
+    {
+        if (typingOn)
+        {
+        
+            typewriter.Play();
+            typingOn = false;
+
+        }
+    
+    }
+    public void ContinueSound()
+    {
+        typingOn = true;
+    }
+    public void MeowSound()
+    {
+        meow.Play();
+        meowOn = true;
+    }
+}
